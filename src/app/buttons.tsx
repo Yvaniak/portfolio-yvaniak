478a6cf04baec406228463ac0bec40ffa73e8840
Yvaniak <yvaniak11@gmail.com>
2024-09-06 02:04:09 +0200

    add: 2 components shacdn carte et boutons arrangés à ma sauce

---
+++ src/app/buttons.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+//ui
+//buttons
+import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+
+//icons
+import { Github } from "lucide-react"
+import { Mail } from "lucide-react"
+import { SiDiscord } from '@icons-pack/react-simple-icons'
+
+export default function Buttons() {
+  return (
+    <div className="space-x-2">
+      <Button asChild>
+        <Link href="https://github.com/Yvaniak">
+          <Github className="mr-2 h-4 w-4"/>
+          Github
+        </Link>
+      </Button>
+      <Button asChild>
+        <Link href="mailto:pro@yvaniak.fr">
+          <Mail className="mr-2 h-4 w-4"/>
+          Email
+        </Link>
+      </Button>
+      <Button asChild>
+        <Link href="https://discord.com/invite/8Xhvt4cm">
+          <SiDiscord className="mr-2 h-4 wy-4 wx-4.5 fill-primary/10 stroke-2 stroke-primary-foreground hover:fill-primary/10"/>
+          Discord
+        </Link>
+      </Button>
+    </div>
+  );
+}
---
+++ src/app/carte.tsx
@@ -0,0 +1,25 @@
+//ui
+//carte
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+export default function Carte() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Yvaniak</CardTitle>
+      </CardHeader>
+      <CardFooter>
+        <p>Software developer</p>
+      </CardFooter>
+    </Card>
+
+  );
+}
