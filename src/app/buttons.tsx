3538ef1a4bc7e5cb875a5a06c24ab1beaec6d2ed
yvaniak <ewen@philippot.biz>
2024-09-07 14:33:36 +0200

    fix: theme chargé après le load pour règler le thème qui revenait au
    défault si on rechargeait

--- src/app/buttons.tsx
+++ src/app/buttons.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button"
 import { Button } from "@/components/ui/button"
 import { useTheme } from "next-themes"
+import { useState, useEffect } from 'react'
 
 //icons
 import { Github } from "lucide-react"
@@ -13,23 +14,36 @@ import { Mail } from "lucide-react"
 import { SiDiscord } from '@icons-pack/react-simple-icons'
 
 export default function Buttons() {
-  const { resolvedTheme } = useTheme();
+  const { theme,resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+   setMounted(true);
+  },[]);
+
+  
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  },[])
 
   return (
     <div className="space-x-2">
-      <Button asChild variant={ resolvedTheme=="dark" ? "default" : "outline"} className="hover:bg-accent hover:text-accent-foreground hover:border ring-offset-background focus-visible:outline-none focus-visible:ring focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"> 
+      <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
         <Link href="https://github.com/Yvaniak">
           <Github className="mr-2 h-4 w-4"/>
           Github
         </Link>
       </Button>
-      <Button asChild variant={ resolvedTheme=="dark" ? "default" : "outline"} className="hover:bg-accent hover:text-accent-foreground hover:border"> 
+      <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
         <Link href="mailto:pro@yvaniak.fr">
           <Mail className="mr-2 h-4 w-4"/>
           Email
         </Link>
       </Button>
-      <Button asChild variant={ resolvedTheme=="dark" ? "default" : "outline"} className="hover:bg-accent hover:text-accent-foreground hover:border"> 
+      <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
         <Link className="fill-accent-foreground/90 stroke-2 stroke-accent" href="https://discord.com/invite/8Xhvt4cm">
           <SiDiscord className="mr-2 h-4 wy-4 wx-4.5 dark:fill-primary/10 stroke-2 dark:fill-accent-foreground dark:stroke-primary-foreground fill-accent-foreground/90 stroke-accent"/>
           Discord
--- src/app/themeButton.tsx
+++ src/app/themeButton.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
+import { useState, useEffect } from 'react'
+
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
@@ -14,11 +16,16 @@ import {
 
 export function ModeToggle() {
   const { resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  },[]);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant={ resolvedTheme=="dark" ? "default" : "outline"} size="icon" className="hover:bg-accent hover:text-accent-foreground dark:border focus-visible:ring-transparent">
+        <Button variant={ !mounted ? "default" : (resolvedTheme=="dark" ? "default" : "outline")} size="icon" className="hover:bg-accent hover:text-accent-foreground dark:border focus-visible:ring-transparent">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
