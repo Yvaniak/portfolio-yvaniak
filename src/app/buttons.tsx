5db0e77fe67e242be330f167cbcfba6b070ac261
yvaniak <ewen@philippot.biz>
2024-09-07 19:17:24 +0200

    add: les liens ouvrent un nouvel onglet au lieu de prendre la place

--- src/app/buttons.tsx
+++ src/app/buttons.tsx
@@ -32,19 +32,19 @@ export default function Buttons() {
   return (
     <div className="space-x-2">
       <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
-        <Link href="https://github.com/Yvaniak">
+        <Link target="_blank" href="https://github.com/Yvaniak">
           <Github className="mr-2 h-4 w-4"/>
           Github
         </Link>
       </Button>
       <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
-        <Link href="mailto:pro@yvaniak.fr">
+        <Link target="_blank" href="mailto:pro@yvaniak.fr">
           <Mail className="mr-2 h-4 w-4"/>
           Email
         </Link>
       </Button>
       <Button asChild variant={ !mounted  ? "default" : (resolvedTheme==="dark" ? "default" : "outline" )} className=" hoverable"> 
-        <Link className="fill-accent-foreground/90 stroke-2 stroke-accent" href="https://discord.com/invite/8Xhvt4cm">
+        <Link target="_blank" className="fill-accent-foreground/90 stroke-2 stroke-accent" href="https://discord.com/invite/8Xhvt4cm">
           <SiDiscord className="mr-2 h-4 wy-4 wx-4.5 dark:fill-primary/10 stroke-2 dark:fill-accent-foreground dark:stroke-primary-foreground fill-accent-foreground/90 stroke-accent"/>
           Discord
         </Link>
