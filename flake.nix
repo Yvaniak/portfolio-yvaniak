730eb8c0758df5206ae008122d0a8473af7b6f11
Yvaniak <yvaniak11@gmail.com>
2024-09-11 21:17:52 +0200

    remplacement ancien truc par flake-utils pour multi-archs

--- flake.nix
+++ flake.nix
@@ -4,36 +4,38 @@
 
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
 
 
-  outputs = { self, nixpkgs }:
-    let
-
-      supportedSystems = [ "x86_64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-
-      formatter = forEachSupportedSystem ({ pkgs }: pkgs.nixpkgs-fmt);
-
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-
-          packages = [
-            pkgs.nodejs_latest
-            pkgs.nodePackages.vscode-langservers-extracted
-            pkgs.nodePackages.pnpm
-          ];
-
-
-          shellHook = ''
-            echo "shell pour portfolio"
-          '';
+  outputs = { self, nixpkgs, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      });
-    };
+      in
+      {
+
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells = {
+          default = pkgs.mkShell {
+
+            packages = [
+              pkgs.nodejs_latest
+              pkgs.nodePackages.vscode-langservers-extracted
+              pkgs.nodePackages.pnpm
+            ];
+
+
+            shellHook = ''
+              echo "shell pour portfolio"
+            '';
+          };
+        };
+      }
+    );
 }
