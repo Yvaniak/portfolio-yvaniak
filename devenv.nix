b813be7b529cb4dd0c27d58c0489c8741c7347b8
Yvaniak <pro@yvaniak.fr>
2025-02-15 20:24:34 +0100

    fix(devenv): use my new modules for ts and nix

--- devenv.nix
+++ devenv.nix
@@ -1,29 +1,16 @@
 {
   pkgs,
+  inputs,
   ...
 }:
 
 {
-  languages.javascript = {
-    enable = true;
-    package = pkgs.nodejs_latest;
-    npm.enable = true;
-    npm.install.enable = true;
-  };
-
-  languages.typescript.enable = true;
-
-  git-hooks.hooks = {
-    eslint.enable = true;
-    biome.enable = true;
-
-    nixfmt-rfc-style.enable = true;
-    statix.enable = true;
-    deadnix.enable = true;
-    commitizen.enable = true;
-  };
-
-  packages = [ pkgs.nil ];
+  imports = [
+    inputs.devenvs.homeManagerModules.devenvs.default
+  ];
+  ts.enable = true;
+  ts.biome.enable = true;
+  nix.enable = true;
 
   enterShell = ''
     test src/app/Inter.ttf || cp "${
